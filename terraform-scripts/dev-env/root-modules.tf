f080c6a832323696f9b6d79b8185126b287b7e5a
zenitugo <ugochiukaegbu19@gmail.com>
2025-03-25 14:13:15 +0100

    subnets variables added to vpc module

--- terraform-scripts/dev-env/root-modules.tf
+++ terraform-scripts/dev-env/root-modules.tf
@@ -2,5 +2,7 @@ module "vpc" {
     source                     = "../sub-script/vpc"
     name                       = var.name
     cidr_block                 = var.cidr_block 
+    private-subnets            = var.private-subnets
+    public-subnets             = var.public-subnets 
   
 }
