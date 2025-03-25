7bb28a2fd005405981efa1d86cbe7ebd7f6ba61b
zenitugo <ugochiukaegbu19@gmail.com>
2025-03-25 14:09:21 +0100

    code to create subnets on VPC

--- terraform-scripts/sub-script/vpc/main.tf
+++ terraform-scripts/sub-script/vpc/main.tf
@@ -14,3 +14,24 @@ resource "aws_vpc" "login-vpc" {
 
 
 ######################### SUBNETS CREATION #####################################
+resource "aws_subnet" "public-subnet" {
+  count = 2 
+  vpc_id     = aws_vpc.login-vpc.id
+  cidr_block = var.public-subnets[count.index]
+  availability_zone = data.aws_availability_zones.available.name[count.index]
+  map_public_ip_on_launch = true
+  tags = {
+    Name = format("public-subnet %d", count.index+1)
+  }
+}
+
+resource "aws_subnet" "private-subnet" {
+  count = 4
+  vpc_id     = aws_vpc.login-vpc.id
+  cidr_block = var.private-subnets[count.index]
+  availability_zone = data.aws_availability_zones.available.name[count.index]
+
+  tags = {
+    Name = format("private-subnet %d", count.index+1)
+  }
+}
