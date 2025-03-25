e81d7e998f8953a9983159f64021ad82264ca40d
zenitugo <ugochiukaegbu19@gmail.com>
2025-03-25 15:20:09 +0100

    code to create route tables

--- terraform-scripts/sub-script/vpc/main.tf
+++ terraform-scripts/sub-script/vpc/main.tf
@@ -88,3 +88,25 @@ resource "aws_nat_gateway" "backend" {
     Name = format("nat-gw %d", count.index+1)
   }
 }
+
+
+##################### ROUTE TABLES CREATION  ################################
+resource "aws_route_table" "public-route-table" {
+  vpc_id = aws_vpc.login-vpc.id
+
+  route {
+    cidr_block           = "0.0.0.0/0"
+    gateway_id           = element(aws_internet_gateway.gw.*.id, count+index)
+  }
+}
+
+
+
+resource "aws_route_table" "private-route-table" {
+  vpc_id = aws_vpc.login-vpc.id
+
+  route {
+    cidr_block           = "0.0.0.0.0/0"
+    nat_gateway_id       = element(aws_nat_gateway.backend.*.id, count+index)
+  }
+}
