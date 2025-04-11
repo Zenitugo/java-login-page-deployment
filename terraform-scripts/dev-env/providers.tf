598e826bd2da27008c12b9a6edfdc7819f56bf70
zenitugo <ugochiukaegbu19@gmail.com>
2025-04-11 03:44:18 +0100

    included s3 deatailsto store terraform state files

--- terraform-scripts/dev-env/providers.tf
+++ terraform-scripts/dev-env/providers.tf
@@ -5,7 +5,16 @@ terraform {
       version = "5.92.0"
     }
   }
+
+  
+  backend "s3" {
+    bucket           = "java-buck-25"
+    key              = var.key_name
+    region           = var.region
+    dynamodb_table   = "java-db-25"
+  }
 }
+
 
 provider "aws" {
   # Configuration options
