fcfe093f4ad35dcad5e65b4acd8c0bcb352f7820
zenitugo <ugochiukaegbu19@gmail.com>
2025-04-13 20:43:01 +0100

    included the frontend subnets variable

--- terraform-scripts/sub-script/vpc/variables.tf
+++ terraform-scripts/sub-script/vpc/variables.tf
@@ -2,4 +2,5 @@ variable "name" {}
 variable "cidr_block" {}
 variable "public_subnets" {}
 variable "private_backend_subnets" {}
+variable "private_frontend_subnets" {}
 variable "private_db_subnets" {}
