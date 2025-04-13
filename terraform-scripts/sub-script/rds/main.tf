e2f6499420c1f8829deef7c638e37b8e8d355a1b
zenitugo <ugochiukaegbu19@gmail.com>
2025-04-13 20:00:45 +0100

    included subnet group to the script

--- terraform-scripts/sub-script/rds/main.tf
+++ terraform-scripts/sub-script/rds/main.tf
@@ -1,3 +1,19 @@
+
+
+# Creation of the subnet group that contains the private subnets created for the database
+resource "aws_db_subnet_group" "rds_subnet_group" {
+  name       = "db-subnet-group"
+  subnet_ids = var.db-subnet  # This should be a list of 2 private subnet IDs
+
+  tags = {
+    Name = "db-subnet-group"
+  }
+}
+
+
+
+
+# Creation of the RDS
 resource "aws_db_instance" "rds" {
   allocated_storage           = 10
   storage_type                = "gp2" 
@@ -11,6 +27,6 @@ resource "aws_db_instance" "rds" {
   skip_final_snapshot         = true
   multi_az                    = true
   publicly_accessible         = false
-  db_subnet_group_name        =  [var.db-subnet][count.index]
-  vpc_security_group_ids      = var.rds-sg-id
+  db_subnet_group_name        = aws_db_subnet_group.rds_subnet_group.id
+  vpc_security_group_ids      = [var.rds_sg_id]
 }
