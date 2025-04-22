6a4d88e4e7e4f18afac3418b2ea7f6dc39c202e7
zenitugo <ugochiukaegbu19@gmail.com>
2025-04-22 17:46:33 +0100

    included loadbalancer module and other variables in other modules

--- terraform-scripts/dev-env/root-modules.tf
+++ terraform-scripts/dev-env/root-modules.tf
@@ -5,6 +5,7 @@ module "vpc" {
     private_backend_subnets    = var.private_backend_subnets
     private_db_subnets         = var.private_db_subnets
     public_subnets             = var.public_subnets
+    private_frontend_subnets   = var.private_frontend_subnets 
   }
 
 module "ec2-template" {
@@ -16,7 +17,6 @@ module "ec2-template" {
     instance_ami_nginx         = var.instance_ami_nginx 
     instance_type              = var.instance_type
     key_name                   = var.key_name
-    key_filename               = var.key_filename    
     sg                         = var.sg
     vpc-id                     = module.vpc.vpc-id 
     rds-sg                     = var.rds-sg 
@@ -31,7 +31,7 @@ module "rds" {
     password                   = var.password
     username                   = var.username 
     db-subnet                  = module.vpc.db-subnet 
-    rds_sg_id                  = module.ec2.rds_sg_id 
+    rds_sg_id                  = module.ec2-template.rds_sg_id 
 }
 
 module "autoscaling" {
@@ -39,9 +39,17 @@ module "autoscaling" {
     frontend-template          = module.ec2-template.frontend-template
     backend-template           = module.ec2-template.backend-template
     backend-subnet             = module.vpc.backend-subnet
-    frontend-subnet            = module.frontend-subnet
+    frontend-subnet            = module.vpc.frontend-subnet
     desired_capacity           = var.desired_capacity
     max_size                   = var.max_size
     min_size                   = var.min_size  
     
+}
+
+
+module "loadbalancer"  {
+    source                     = "../sub-script/loadbalancers"
+    public-subnet              = module.vpc.public-subnet
+    vpc-id                     = module.vpc.vpc-id
+    asg                        = module.autoscaling.asg 
 }
