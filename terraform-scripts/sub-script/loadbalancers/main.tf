0706e47be6566da26de94326d5a6603c2088fe1b
zenitugo <ugochiukaegbu19@gmail.com>
2025-05-27 17:39:54 +0100

    included load balancer configuration for the backend ec2's

--- terraform-scripts/sub-script/loadbalancers/main.tf
+++ terraform-scripts/sub-script/loadbalancers/main.tf
@@ -1,4 +1,4 @@
- ################## Network load balancer creation ######################
+ ################## Network load balancer creation For the Frontend EC2's ######################
 
  resource "aws_lb" "nlb" {
   name               = "app-nlb"
@@ -37,10 +37,59 @@ resource "aws_lb_listener" "nginx_listener" {
 
 ############### Create a new ALB Target Group attachment ##################
 resource "aws_autoscaling_attachment" "attachment" {
-  autoscaling_group_name = var.asg
+  autoscaling_group_name = var.frontend_asg
   lb_target_group_arn    = aws_lb_target_group.nginx_tg.arn
 }
 
 
 
 
+############################# Application load balancer for the Backend EC2's #########################
+
+ resource "aws_lb" "backend_nlb" {
+  name               = "backend-nlb"
+  internal           = true
+  load_balancer_type = "application"
+  subnets            = var.backend-subnet
+
+  tags = {
+    Environment = "dev"
+  }
+}
+
+
+
+########## Target Group Creation ####################
+resource "aws_lb_target_group" "backend_tg" {
+  name     = "backend-target-group"
+  port     = 8080
+  protocol = "HTTP"
+  vpc_id   = var.vpc-id
+  target_type = "instance" # or "ip"
+}
+
+#################### Target Group Listener ################
+resource "aws_lb_listener" "backend_listener" {
+  load_balancer_arn = aws_lb.backend_nlb.arn
+  port              = 8080
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.backend_tg.arn
+  }
+}
+
+
+############### Create a new ALB Target Group attachment ##################
+resource "aws_autoscaling_attachment" "backend_attachment" {
+  autoscaling_group_name = var.backend_asg
+  lb_target_group_arn    = aws_lb_target_group.backend_tg.arn
+}
+
+
+# Store the backend load balancer
+resource "aws_ssm_parameter" "backend_endpoint" {
+    name  = "/app/backend_endpoint"
+    value = aws_lb.backend_nlb
+}
